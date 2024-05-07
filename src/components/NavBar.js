48a51482623fd9360d0c40cac6c66b5836567f1b
Sophie Carr <sacarr98@gmail.com>
2024-05-07 12:08:04 +0000

    import bootstrap navbar element

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from '../assets/logo.png'
 
 const NavBar = () => {
-    return <div>navbar placeholder</div>;
+    return (
+        <Navbar expand="md" fixed="top">
+            <Container>
+                <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link>Home</Nav.Link>
+                    <Nav.Link>Sign in</Nav.Link>
+                    <Nav.Link>Sign up</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavBar;

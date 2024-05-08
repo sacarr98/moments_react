184aba9bd01dd49b77cbffc2f079003492158e53
Sophie Carr <sacarr98@gmail.com>
2024-05-08 10:55:45 +0000

    add font-awesome icons to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,16 @@ const NavBar = () => {
                 <Navbar.Brand><img src={logo} alt="logo" height="45" /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link>Home</Nav.Link>
-                    <Nav.Link>Sign in</Nav.Link>
-                    <Nav.Link>Sign up</Nav.Link>
+                <Nav className="ml-auto text-left">
+                    <Nav.Link>
+                        <i className="fas fa-home"></i>Home
+                        </Nav.Link>
+                    <Nav.Link>
+                    <i className="fas fa-sign-in-alt"></i>Sign in
+                        </Nav.Link>
+                    <Nav.Link>
+                    <i className="fas fa-user-plus"></i>Sign up
+                        </Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>

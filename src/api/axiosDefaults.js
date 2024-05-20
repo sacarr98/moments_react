d8edebb909661b59d79c0d5b6a615b4d80896f23
Sophie Carr <sacarr98@gmail.com>
2024-05-20 16:14:09 +0000

    create signup form error recognition

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://moments-react-proj-af2ae8d26ea8.herokuapp.com'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
